3979121be607eab7452b894233acf65b3566a215
EX1A <silverskinx@gmail.com>
2023-06-11 12:33:43 -0400

    added method to uint8.ts to return signed representation of current value.

--- primitives/uint8.ts
+++ primitives/uint8.ts
@@ -21,4 +21,8 @@ export class Uint8{
     public leftShiftWithCarry(shiftByN: number) {
         //?
     }
+
+    public getSignedRepresentation(): number {
+        return this._value << 24 >> 24;
+    }
 }
