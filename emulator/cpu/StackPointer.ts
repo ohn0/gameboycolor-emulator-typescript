2578dec9a88b2692d4240fb82909dd78c76195f3
EX1A <silverskinx@gmail.com>
2023-06-11 12:34:12 -0400

    Modified StackPointer to extend HiLoRegister so that the MSB and LSB can be accessed

--- emulator/cpu/StackPointer.ts
+++ emulator/cpu/StackPointer.ts
@@ -1,21 +1,12 @@
-import { Register16Bit } from "./register16bit";
+import { HiLoRegister } from "./HiLoRegister";
+import { Register8bit } from "./register";
 
-export class StackPointer extends Register16Bit {
-    constructor(registerName: string) {
-        super(registerName);
+export class StackPointer extends HiLoRegister {
+    constructor(lsb : number, msb : number, name : string) {
+        super(new Register8bit(lsb), new Register8bit(msb), name);
     }
 
-    public setStackValue(value: number) {
-        this.value = value;
-    }
+    public setStackValue(value: number) { this.setRegister(value); }
 
-    public getStackValue(): number { return this.value; }
-    
-    public override setRegister(value: number) {
-        this.setStackValue(value);
-    }
-
-    public getRegister(): number {
-        return this.getStackValue();
-    }
+    public getStackValue(): number { return this.getRegister(); }
 }
