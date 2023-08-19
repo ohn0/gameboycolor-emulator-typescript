f2c2b0996353a49f3080864e4d422f5534fd72a7
EX1A <silverskinx@gmail.com>
2023-08-19 09:59:12 -0400

    Added logger

--- logger/logger.ts
+++ logger/logger.ts
@@ -1,6 +1,15 @@
+import { opcodes } from './../resources/Opcodes';
 import { HiLoRegister } from "../emulator/cpu/HiLoRegister";
 import { Register8bit } from "../emulator/cpu/register";
 import * as fs from 'fs';
+import * as  path from 'path';
+
+type opcodeBase = {
+    opcode: number | undefined,
+    mnemonic: string | undefined,
+    operands: string | undefined,
+    isPrefixed: boolean
+}
 
 export class Logger{
     private _step = 0;
@@ -13,9 +22,18 @@ export class Logger{
 
     private messages: Array<string>;
     private message: string;
+
+    private opcodesTrace: Array<{
+        traceMessage: string,
+        code: opcodeBase
+    }>;
+
     constructor() {
         this.messages = new Array<string>();
         this.message = '';
+
+        this.opcodesTrace = new Array<{
+            traceMessage: string, code: opcodeBase}>();
     }
 
     configureLogging(stepVal : number) {
@@ -59,4 +77,56 @@ export class Logger{
         this.messages = new Array<string>();
     }
 
+    logOpCode(opcode: number, isPrefixed = false) {
+        const hexOpcode = `0x${opcode.toString(16).toUpperCase()}`;
+        const opCodeDetails =
+            isPrefixed
+                ? opcodes.cbprefixed.find(o => o.code == opcode)
+                : opcodes.unprefixed.find(o => o.code == opcode);
+        this.opcodesTrace.push({
+            traceMessage: `opcode ${hexOpcode} executed, ${opCodeDetails?.mnemonic}`,
+            code: {
+                opcode: opCodeDetails?.code,
+                mnemonic: opCodeDetails?.mnemonic,
+                operands: opCodeDetails?.operands.toString(),
+                isPrefixed : isPrefixed
+            }
+        });
+    }
+
+    logOpCodesToFile() {
+        let output = ''
+        let uniqueOpcodeOutput = '';
+        let uniquePrefixedOpcodeOutput = '';
+        const uniqueOpcodes = new Array<opcodeBase>();
+        const uniquePrefixedOpCodes = new Array<opcodeBase>();
+        this.opcodesTrace.forEach(o => {
+            if (!o.code.isPrefixed && uniqueOpcodes.find(u => o.code.opcode == u.opcode) == undefined) {
+                if (o.code.opcode != undefined) {
+                        uniqueOpcodes.push(o.code);
+                }
+            }
+
+            if (o.code.isPrefixed && uniquePrefixedOpCodes.find(up => o.code.opcode == up.opcode) == undefined) {
+                if (o.code.opcode != undefined) {
+                    uniquePrefixedOpCodes.push(o.code);
+                }
+            }
+            output += o.traceMessage + '\n'
+        });
+        // uniqueOpcodes = uniqueOpcodes.sort((a, b) => {
+        //     if (a.opcode != undefined && b.opcode != undefined) {
+        //         return a.opcode = b.opcode;
+        //     }
+        //     return 0;
+        // });
+        uniqueOpcodes.forEach(u =>
+            uniqueOpcodeOutput += `0x${u.opcode?.toString(16).toLocaleUpperCase().padStart(2, '0')}: ${u.mnemonic}` + '\n');
+        
+        uniquePrefixedOpCodes.forEach(u =>
+            uniquePrefixedOpcodeOutput += `0x${u.opcode?.toString(16).toLocaleUpperCase().padStart(2, '0')}: ${u.mnemonic}` + '\n');
+        fs.writeFileSync(path.resolve(__dirname,'..\\logOutput', "opcodeTrace.yaml"), output);
+        fs.writeFileSync(path.resolve(__dirname,'..\\logOutput', "uniqueOpCodes.yaml"), uniqueOpcodeOutput);
+        fs.writeFileSync(path.resolve(__dirname,'..\\logOutput', "uniquePrefixedOpCodes.yaml"), uniquePrefixedOpcodeOutput);
+    }
 }
