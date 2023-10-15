645780a605f06fbdef36a61323f7cf4987ebd036
EX1A <silverskinx@gmail.com>
2023-10-15 15:52:37 -0400

    update loading roms

--- emulator/romLoader.ts
+++ emulator/romLoader.ts
@@ -1,9 +1,16 @@
 import * as fs from 'fs';
 import * as path from 'path';
-
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 export class RomLoader{
-    static load(fileName: string): Uint8Array {
-        const rom = fs.readFileSync(path.resolve(__dirname,'..', fileName));
+    static load(fileName: string, isStandAlone = false): Uint8Array {
+        let rom: Buffer;
+        if (isStandAlone) {
+            rom = fs.readFileSync(path.resolve(dirname(fileURLToPath(import.meta.url)),'..', fileName));
+        }
+        else {
+            rom = fs.readFileSync(path.resolve(__dirname,'..', fileName));            
+        }
         const RAM: Uint8Array = new Uint8Array(rom.length);
         let idx = 0;
         for (const b of rom) {
